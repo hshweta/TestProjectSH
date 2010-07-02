17ac6bbb3f201b6a1310546084e542c9414e7be6
Shwetambari Helwade <shweta.helwade@gmail.com>
2010-07-02 17:33:24 +0530

    modifying one file and adding new file at the same time

--- hello_world.rb
+++ hello_world.rb
@@ -1,3 +1,5 @@
 puts "Hello World! Exploring Git.. :)"
 
 puts "Adding some extra text to see how diff looks.."
+
+puts "some more text, some more text, some more text, some more text, some more text"
