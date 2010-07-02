6dbcc27ac01bd93c8257258ef869a3a9a5ea1396
Shwetambari Helwade <shweta.helwade@gmail.com>
2010-07-02 17:36:52 +0530

    removed extra text

--- hello_world.rb
+++ hello_world.rb
@@ -2,4 +2,3 @@ puts "Hello World! Exploring Git.. :)"
 
 puts "Adding some extra text to see how diff looks.."
 
-puts "some more text, some more text, some more text, some more text, some more text"
